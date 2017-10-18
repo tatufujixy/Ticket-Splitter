fa8e818e807f9ab32d41a5660c69e8685bc76640
6315013 <6315013@nodab016.ed.tus.ac.jp>
2017-10-18 17:38:12 +0900

    GUI追加

---
+++ src/jp/ac/tus/ed/ticketsplitter/gui/Gui.java
@@ -0,0 +1,58 @@
+package jp.ac.tus.ed.ticketsplitter.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+	
+	class Gui extends JFrame implements ActionListener{
+		
+		JLabel testlabel = new JLabel();
+		 JTextField tex1 = new JTextField(20);
+		    JTextField tex2 = new JTextField(20);
+		    
+	  public static void main(String args[]){
+	    Gui frame = new Gui("�o�H����");
+	    frame.setVisible(true);
+	  }
+
+	  Gui(String title){
+	    setTitle(title);
+	    setBounds(100, 100, 300, 300);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	    JLabel label1 = new JLabel("��ԉw");
+	    JLabel label2 = new JLabel("�~�ԉw");
+	    
+
+	 	    
+	    JPanel p = new JPanel();
+	   
+	    JButton button = new JButton("���s");
+	    
+	    
+	    p.add(label1);
+	 
+	    p.add(tex1);
+	    
+	    p.add(label2);
+	    
+	    p.add(tex2);
+	
+	    p.add(button);
+	    button.addActionListener(this);
+	    
+	    Container contentPane = getContentPane();
+	    contentPane.add(p, BorderLayout.CENTER);
+	    contentPane.add(testlabel, BorderLayout.SOUTH);
+	  }
+	  
+	  public void actionPerformed(ActionEvent e){
+		    testlabel.setText(tex1.getText() + tex2.getText());
+		  }
+	  
+	}
+
+
