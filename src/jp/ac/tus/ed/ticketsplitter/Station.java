64378d2645dcb5ad3b9ae5af00e99356436730c4
Tatu Fujixy <6315101@ed.tus.ac.jp>
2017-10-24 11:50:18 +0900

    Stationと、Database.getStationを実装

--- src/jp/ac/tus/ed/ticketsplitter/Station.java
+++ src/jp/ac/tus/ed/ticketsplitter/Station.java
@@ -1,6 +1,8 @@
 package jp.ac.tus.ed.ticketsplitter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,51 +21,102 @@ public class Station {
 	public static final int SUBURB_TOKYO=31;
 	public static final int SUBURB_OSAKA=32;
 	//・・・
-
+	
+	
+	String name;
+	int id;
+	Map<Integer,Integer> stationIdOfLine=new HashMap<Integer,Integer>();//<line_id,station_id>
+	Map<Integer,List<Integer>> nextStationId=new HashMap<Integer,List<Integer>>();
+	int specificWardsAndCities=0;
+	boolean inYamanoteLine;
+	boolean inOsakaKanjoLine;
+	int specificArea=0;
+	int suburbArea=0;
+	Map<Integer,BigDecimal> distance=new HashMap<Integer,BigDecimal>();
+	
+	
 	
 	//コンストラクタはDatabaseで呼ばれる
 	
-	//test
+	void setName(String s){
+		name=s;
+	}
+	public String getName(){
+		return name;
+	}
+	void setStationId(int i){
+		id=i;
+	}
 	public int getStationId(){
 	//駅ID。乗換可能駅であればその共通のID、すなわちDBのid_station
-		return -1;
+		return id;
+	}
+	void setStationIdOfLine(int line,int station){
+		stationIdOfLine.put(line, station);
 	}
 	public int getStationIdOfLine(int lineId){
 	//乗換可能駅での、路線IDがlineIdのレコードでのID
 	//単独駅では必ずgetStationId()と同じ値になる
-		return -1;
+		return stationIdOfLine.get(lineId);
 	}
-	public Map<Integer,Integer> nextStationId(){
+	void addNextStationId(int line,int nextStation){
+		if(nextStation==0){
+			return;
+		}
+		if(!nextStationId.containsKey(line)){
+			nextStationId.put(line, new ArrayList<Integer>(2));
+		}
+		nextStationId.get(line).add(nextStation);
+	}
+	public Map<Integer,List<Integer>> nextStationId(){
 	//隣の駅への路線idと駅idの組
 	//次の駅に行くのに経由する路線idをKeyに、次の駅の駅idをValueにして入れている。すなわち、Map<路線id,駅id>
-		return null;
+		return nextStationId;
+	}
+	void setSpecificWardsAndCities(int s){
+		specificWardsAndCities=s;
 	}
 	public int getSpecificWardsAndCities(){
 	//特定都区市内ならその値を返す。そうでないなら0を返す
-		return -1;
+		return specificWardsAndCities;
+	}
+	void setIsInYamanoteLine(boolean b){
+		inYamanoteLine=b;
 	}
 	public boolean isInYamanoteLine(){
 	//山の手線内ならtrue
-		return false;
+		return inYamanoteLine;
+	}
+	void setInOsakaKanjoLine(boolean b){
+		inOsakaKanjoLine=b;
 	}
 	public boolean isInOsakaKanjoLine(){
 	//大阪環状線内ならtrue
-		return false;
+		return inOsakaKanjoLine;
+	}
+	void setSpecificArea(int i){
+		specificArea=i;
 	}
 	public int getSpecificArea(){
 	//特定区間内ならその値を返す。そうでないなら0を返す
-		return -1;
+		return specificArea;
+	}
+	void setSuburbArea(int i){
+		suburbArea=i;
 	}
 	public int getSuburbArea(){
 	//近郊区間内ならその値を返す。そうでないなら0を返す
-		return -1;
+		return suburbArea;
+	}
+	void setDistance(int line, BigDecimal bd){
+		distance.put(line, bd);
 	}
 	public BigDecimal getDistance(int line){
 	//路線idがlineの路線の、起点駅からこの駅までの営業キロを返す
-		return null;
+		return distance.get(line);
 	}
 	public List<Integer> getLineId(){
 	//この駅が属する路線idのリストを返す
-		return null;
+		return new ArrayList<Integer>(stationIdOfLine.keySet());
 	}
 }
