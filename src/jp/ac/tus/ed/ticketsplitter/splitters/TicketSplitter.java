a9189be256bf8d01904a8e1e12b4637f8fd5ad25
6315013 <6315013@nodag030.ed.tus.ac.jp>
2017-10-24 19:46:21 +0900

    草案。

--- src/jp/ac/tus/ed/ticketsplitter/splitters/TicketSplitter.java
+++ src/jp/ac/tus/ed/ticketsplitter/splitters/TicketSplitter.java
@@ -21,16 +21,30 @@ public class TicketSplitter {
 	//中間発表までには、ダイクストラ法で経路を求めたあと、その運賃を求めて返すこととする(戻り値のリストの長さは1)
 		return null;
 	}
+	
 	public static Route dijkstra(Station start,Station dest){
+		int startid = start.getStationId();
 	//ダイクストラ法により、start駅からdest駅までの経路を求める
 	//FareCalculatorから呼ばれるかも
 		//処理中の駅
 		StaNode processingStaNode;
 		Station processingSta;
-		List<Integer> processingstationslineid;
-		int processinglineid;
-		Map<Integer,List<Integer>> processingstationsmap;
+		List<Integer> processingstations_lineids;
+		int processing_lineid;
+		Map<Integer,List<Integer>> processingstations_map;
 		List<Integer> nextstations;
+		//処理中の駅
+		Station newstation;
+		StaNode newnode;
+		//駅間距離用の変数
+		BigDecimal betweensta;
+		BigDecimal absbetween;
+		//start dest間距離用変数
+		BigDecimal startdest ;
+		
+		//処理終了のためのフラグ
+		boolean endprocess = true;
+		
 		
 		//最短距離未確定リスト unsettled 未確定の意
 		ArrayList<StaNode> unsettled = new ArrayList<StaNode>();
@@ -40,8 +54,8 @@ public class TicketSplitter {
 		//未確定リストにdist0を入れる
 		 unsettled.add(new StaNode(dest,new BigDecimal("0,0"),null));
 		
-		 //ここから下をstartStaNodeが出るまでループ
-		 
+		 //ここから下をstart StaNodeが出るまでループ
+		while(endprocess){
 		 //最短距離未確定リストをコスト順に並べる
 		 //ListComparatorクラスの条件に従いソート
 		 Collections.sort(unsettled,new ListComparator());
@@ -49,44 +63,64 @@ public class TicketSplitter {
 		//取り出した駅(unsettled.get(0))が確定済みリストにあれば無視、未確定リストにあれば、距離を計算しなおし、短ければ距離・直前の駅を更新
 		 processingStaNode = unsettled.get(0);
 		 if(committed.indexOf(unsettled.remove(0))==-1){
-			 //確定済みリストにない場合の処理
+			 //確定済みリストにない場合の処理（入れるリスト分けではじいてるからある場合はあり得ないはず）
 			 //処理する駅を取り出す
 			 processingSta = processingStaNode.getSta();
 			 //その駅の路線idをとる
-			 processingstationslineid = processingSta.getLineId();
+			 processingstations_lineids = processingSta.getLineId();
 			 //その駅のマップを取り出す。取り出した路線idをもとに隣接駅をマップから取り出す。
-			 processingstationsmap = processingSta.nextStationId();//戻り値がマップの形
-			 while(!(processingstationslineid.isEmpty())){
+			 processingstations_map = processingSta.nextStationId();//戻り値がマップの形
+			 
+			 while(!(processingstations_lineids.isEmpty())){
 				 //ある路線の、隣接駅をすべて抜き出しノードにする。取り出した路線idは消す。
-				 processinglineid = processingstationslineid.get(0);
-				 processingstationslineid.remove(0);
-				 //隣接駅を路線idをもとにマップから取り出す。当然2つ要素を持つ。
-				 nextstations = processingstationsmap.get(processinglineid);
+				 processing_lineid = processingstations_lineids.get(0);
+				 processingstations_lineids.remove(0);
+				 //隣接駅を路線idをもとにマップから取り出す。当然2つ要素を持つintegerのリスト。
+				 //nextstationsは両隣の駅のidを格納しているリスト。
+				 nextstations = processingstations_map.get(processing_lineid);
 				 while(!(nextstations.isEmpty())){
 					 //駅を作成、それをもとにノードを作成し、unsettledに入れるかどうか確認後入れる。
-					Database.getStation(nextstations.get(0));
+					newstation = Database.getStation(nextstations.get(0));
 					nextstations.remove(0);
-				 }
-				 
-			 }
-			 
-		 };
-		 committed.add(unsettled.remove(0));
-		//上の処理を、start駅が確定済みリストに入るまで繰り返す
-		
-		
-		
-		
-		
-		//処理内容、隣接している駅名をstationをもとに呼び出し、それまでのdistanceと隣接駅までの距離を足したものを
-		//いれる。処理済みリストにその駅名があるならそのノードは削除。ないなら処理待ちリストに。
-		//処理済みかどうかの判定にdest駅と一致しているかもいれておく。一致したら終了。
-		//距離を算出。
-		
+					//駅間の距離を算出し絶対値をつける
+					betweensta = ((processingStaNode.getSta()).getDistance(processing_lineid)).subtract(newstation.getDistance(processing_lineid));
+					absbetween = betweensta.abs();
+					newnode = new StaNode(newstation,absbetween.add(processingStaNode.getdist()),processingStaNode);
+					//新しく作ったStaNodeがstart、未確定内、確定内にあるか確認。
+					if(!(committed.indexOf(newnode)==-1)){
+						//最短経路が確定しているリスト内にあったならばこのノードは破棄。なにもしない。
+					}else if(newstation.getStationId() == startid){
+						//start stationと一致しているならば処理を終了
+						//求めたい距離はstartのstationに一致したノードを求めたStaNodeに書いてあるdistと、
+						//そこからstartまでの距離をたしたものなので
+						startdest = (absbetween).add(processingStaNode.getdist());
+						//経路はbackがnullになるまでnodeを追えばわかる。未実装
+						
+						endprocess = false;
+						
+					}else if(!(unsettled.indexOf(newnode)==-1)){
+						//未確定リストにあったならば距離を確認。更新するか否か
+						StaNode alreadynode = unsettled.remove(unsettled.indexOf(newnode)); 
+						if((alreadynode.getdist()).compareTo(newnode.getdist()) > 0){
+							// alreadyのほうが大きいならばnewnodeを格納、そうでないならalreadynodeを再格納。
+							unsettled.add(newnode);
+						}else{
+							unsettled.add(alreadynode);
+							}
+						}
+					//未確定、確定、startnodeの条件に合わない場合は単に未確定リストに追加。
+						unsettled.add(newnode);
+					}
+			 }	
+		 }
+//この下がwhileの}	
+		}
+
+		//処理が終わり次第 返し値Routeクラスのインスタンス？
 		return null;
+	
+	
 	}
-	
-	
 }
 
 //nodeクラス
@@ -106,6 +140,9 @@ class StaNode{
 	}
 	public Station getSta(){
 		return this.sta;
+	}
+	public StaNode getback(){
+		return this.back;
 	}
 	@Override
 	public boolean equals(Object o){
