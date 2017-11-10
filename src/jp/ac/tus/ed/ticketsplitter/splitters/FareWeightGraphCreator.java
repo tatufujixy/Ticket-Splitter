8d3c93fcf5c5cb4d9c6470c013c371ec69903fd6
6315056 <6315056@nodab038.ed.tus.ac.jp>
2017-11-10 18:00:03 +0900

    メソッドのメインを追加（未完成）

--- src/jp/ac/tus/ed/ticketsplitter/splitters/FareWeightGraphCreator.java
+++ src/jp/ac/tus/ed/ticketsplitter/splitters/FareWeightGraphCreator.java
@@ -10,7 +10,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.List;
+
+import jp.ac.tus.ed.ticketsplitter.Database;
+import jp.ac.tus.ed.ticketsplitter.Station;
 
 public class FareWeightGraphCreator {
 	
@@ -28,10 +32,34 @@ public class FareWeightGraphCreator {
 			e.printStackTrace();
 		}
 	}
+	
+	/* public static giveData(start_id, dest_id, fare){
+		
+	} */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO 自動生成されたメソッド・スタブ
-
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		Station start=null;
+		while(start==null){
+			System.out.println("乗車駅を入力:");
+			String str = in.readLine();
+			start = Database.getStation(str);
+		}
+		
+		Station dest = null;
+		while(dest==null){
+			System.out.println("降車駅を入力:");
+			String str = in.readLine();
+			dest = Database.getStation(str);
+		}
+		
+		// List<Fare> list = (切符1枚で買った場合の値段を出すプログラミング名);
+		
+		
+		
 	}
 
 }
