11fba2180f513041824af6007028ee3691e50d39
Tatu Fujixy <6315101@ed.tus.ac.jp>
2017-11-30 12:10:55 +0900

    デバッグ

--- src/jp/ac/tus/ed/ticketsplitter/Debug.java
+++ src/jp/ac/tus/ed/ticketsplitter/Debug.java
@@ -21,7 +21,7 @@ public class Debug {
 		}
 		*/
 		
-		System.out.println("dijkstraのデバッグ");
+		System.out.println("dijkstraとFareCalculatorのデバッグ");
 		
 		Station start=null;
 		while(start==null){
@@ -41,6 +41,17 @@ public class Debug {
 		for(Station s:route.getStationsList()){
 			System.out.print(s.getName()+" ");
 		}
+		System.out.println("\n");
+
+		Ticket t=new FareCalculator().calculate(route);
+		Route r=t.getRoute();
+		System.out.println(t.getStart()+" -> "+t.getDestination());
+		System.out.println("運賃:"+t.getFare()+"円 ("+t.getFareCategory()+")");
+		System.out.print("経路 : ");
+		for(String str : r.via()){
+			System.out.print(str+"  ");
+		}
+		System.out.println("\n");
 	}
 
 }
